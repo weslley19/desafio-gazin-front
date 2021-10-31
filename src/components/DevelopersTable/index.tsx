cbecf54e9a293d068f4b84a01ff6443e003a28a4
Weslley Oliveira <weslleyoliveira258@gmail.com>
2021-10-31 01:11:38 -0300

    feat: adding icons on edit, delete and show

--- src/components/DevelopersTable/index.tsx
+++ src/components/DevelopersTable/index.tsx
@@ -1,4 +1,6 @@
 import { useTransactions } from "../../hooks/useTransactions";
+import { FaUserEdit, FaSearchPlus, FaRegTrashAlt } from 'react-icons/fa';
+
 import { Container } from "./styles";
 
 export function DevelopersTable() {
@@ -11,26 +13,28 @@ export function DevelopersTable() {
       <table>
         <thead>
           <tr>
-            <th>Ações</th>
             <th>Nome</th>
             <th>Sexo</th>
             <th>Idade</th>
             <th>Hobby</th>
             <th>Data de aniversário</th>
+            <th>Ações</th>
           </tr>
         </thead>
         <tbody>
           {developers.length ? (
             developers.map(developer => (
               <tr key={developer.id}>
-                <td>
-                  <a href="#">Visu</a> | <a href="#">Edit</a>
-                </td>
                 <td>{developer.name}</td>
                 <td>{developer.sexo === 'M' ? 'Masculino' : 'Feminino'}</td>
                 <td>{`${developer.age} anos`}</td>
                 <td>{developer.hobby}</td>
                 <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(developer.birthdate))}</td>
+                <td className="">
+                  <a href="http://localhost:3000/"><FaSearchPlus color="#000" /></a> &nbsp;&nbsp;
+                  <a href="http://localhost:3000/"><FaUserEdit color="#000" /></a> &nbsp;&nbsp;
+                  <a href="http://localhost:3000/"><FaRegTrashAlt color="#be0404" /></a>
+                </td>
               </tr>
             ))
           ) : null
